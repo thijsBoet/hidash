2bd5576f429855f16657d46721e5cee69ed98ec7
thijsBoet <m.boet@chello.nl>
2022-12-11 15:06:56 +0100

    used Mocha instead of using self written test

--- index.test.js
+++ index.test.js
@@ -1,18 +1,29 @@
+const assert = require('assert');
+
 const { forEach, map } = require('./index');
 
-let sum = 0;
-forEach([1, 2, 3], value => {
-	sum+= value;
+// const test = (desc, fn) => {
+// 	console.log('----', desc, '----');
+// 	try {
+// 		fn();
+// 	} catch (err) {
+// 		console.error(err.message);
+// 	}
+// };
+
+it('The forEach function', () => {
+	let sum = 0;
+	forEach([1, 2, 3], value => {
+		sum += value;
+	});
+
+	assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
-if(sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-}
+it('The map function', () => {
+	const result = map([1, 2, 3], value => {
+		return value * 2;
+    });
 
-const result = map([1, 2, 3], value => {
-    return value * 2;
+    assert.deepStrictEqual(result, [2, 4, 6]);
 });
-
-if(result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`);
-}
